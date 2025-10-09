88e88c72e69a3d7d30d45a3ee6785305df42dd56
Andrea Conti <andreaconti05@gmail.com>
2025-10-09 19:15:37 +0200

    aggiunto test in POST nel backend e testato con postman

--- server/server.js
+++ server/server.js
@@ -3,31 +3,34 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/ProgettoReact')
-.then(()=>{
+mongoose
+  .connect("mongodb://localhost:27017/ProgettoReact")
+  .then(() => {
     console.log("connesso al db!");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log("impossibile connettersi al db!", err);
-})
+  });
 
 const schema = new mongoose.Schema({
-    name: String,
-    cognome: String,
-    imgURL: String,
-    isCoglione: String,
+  name: String,
+  cognome: String,
+  imgURL: String,
+  isCoglione: Boolean,
 });
 
-const Model = mongoose.model('utenti',schema);
+const Model = mongoose.model("utenti", schema);
 
-const inserUser = async(obj)=>{
-    const user = new Model(obj)
+const inserUser = async (obj) => {
+  const user = new Model(obj);
 
-    await user.save();
-}
+  await user.save();
+};
 
-
+/* SCRITTURA SU DB E LETTURA
 const leggiUtenti = async () => {
     try {
         const utenti = await Model.find({});
@@ -40,7 +43,7 @@ const leggiUtenti = async () => {
 const main = async () => {
     await inserUser({
         name: 'prov2',
-        cognome: 'tua mamma',
+        cognome: '',
         imgURL: '',
         isCoglione: true
     });
@@ -48,4 +51,22 @@ const main = async () => {
 }
 
 
- main();
+ main(); */
+
+const Form = mongoose.model("Utenti", schema);
+
+// Endpoint per ricevere dati dal form
+app.post("/api/forms", async (req, res) => {
+  try {
+    console.log("ricevuto:", req.body); // << utile per debug!
+    const formData = new Form(req.body);
+    const savedData = await formData.save();
+    res.status(201).json(savedData);
+    console.log("fatto");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Avvio server
+app.listen(5000, () => console.log("Server avviato sulla porta 5000"));
